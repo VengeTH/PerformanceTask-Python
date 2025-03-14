f30a5d898ff257a3e8ec87989be7be22e0cd261e
VengeTH <prinzemikhailsadsad@gmail.com>
2025-03-14 16:39:21 +0800

    added parenthesis logic

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 #
 #EOF (end-of-file) token is used to indicate that
 #there is no more input left for lexical analysis
-INTEGER, PLUS, MINUS, MUL, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = ('INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF')
 
 class Token(object):
     def __init__(self, type, value):
@@ -89,6 +89,14 @@ class Lexer(object):
                 self.advance()
                 return Token(DIV, '/')
 
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+
             self.error()
 
         return Token(EOF, None)
@@ -122,8 +130,14 @@ class Interpreter(object):
     def factor(self):
         """Return an INTEGER token value"""
         token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            result = self.expr()
+            self.eat(RPAREN)
+            return result
 
     # def advance(self):
     #     """Advance the 'pos' pointer and set 'current_char' variable."""
